4167ba9420e036f9811d5193120b0126a248f7a9
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2017-09-04 16:32:53 -0300

    Criação das classes Categotia e Produto

---
+++ DDDDevMedia/Dominio.DDDDevMedia/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.DDDDevMedia
+{
+    public class Categoria
+    {
+        
+        // Propriedades
+        public int Id_Categoria { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public IList<Produto> Produtos { get; set; }
+    }
+
+}
